9f2be904310dd68ca8c59c416a0e5fedfdbc616c
keita-tbt <keita.oaku@gmail.com>
2020-06-08 19:52:59 +0900

    [issue_002] Localization
    add i18n

--- FRONT/vtuber-urn/src/app/app.module.ts
+++ FRONT/vtuber-urn/src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +18,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+    },
+      defaultLanguage: 'jp',
+      isolate: false
+  })
   ],
   providers: [],
   bootstrap: [AppComponent]

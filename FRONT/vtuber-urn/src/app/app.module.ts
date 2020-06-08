a6203141c2a357c8a3ed399e55ec8d961fbd0593
keita-tbt <keita.oaku@gmail.com>
2020-06-08 21:38:00 +0900

    create main page
    frame added

--- FRONT/vtuber-urn/src/app/app.module.ts
+++ FRONT/vtuber-urn/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
-import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
+// primeng
+import { CardModule } from 'primeng/card';
+import { MenuModule } from 'primeng/menu';
+import { TabMenuModule } from 'primeng/tabmenu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -14,7 +20,9 @@ export function createTranslateLoader(http: HttpClient) {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TopMenuComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +36,11 @@ export function createTranslateLoader(http: HttpClient) {
     },
       defaultLanguage: 'jp',
       isolate: false
-  })
+  }),
+  // primeng modules
+  CardModule,
+  MenuModule,
+  TabMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
